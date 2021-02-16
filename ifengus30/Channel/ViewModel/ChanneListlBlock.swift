d28d772d9fd947ccdb51daf93b25be4b98cb73ae
wwwendydev <us68design@gmail.com>
2021-02-15 22:46:45 -0800

    调整栏目分类页面

--- ifengus30/Channel/ViewModel/ChanneListlBlock.swift
+++ ifengus30/Channel/ViewModel/ChanneListlBlock.swift
@@ -10,56 +10,40 @@ import SwiftUI
 
 struct ChanneListlBlock: View {
   @ObservedObject var channelmanager = ChannelManager()
-
-    
     
   var body: some View {
-    VStack(alignment: .leading) {
-      Text("栏目分类")
-        .font(.title3)
-        .bold()
-      
-      ForEach(channelmanager.channels) {channel in
-        Divider()
-        //AppSmall1Block()
-        
-        // 目录ID循环
-        HStack {
-         Image(systemName: "dot.circle.and.cursorarrow")
-            .frame(width: 24, height: 24)
-            .accentColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-
-          VStack(alignment: .leading) {
-            NavigationLink(destination:ChannelListView( channelID: channel.id )){
-                Text("\(channel.name)")
-                         }
-            .font(.system(size: 16))
-          }
-
-          Spacer()
-
-          VStack {
-            
-            Button("查看子目录 >") {}
-              .font(Font.system(.caption).bold())
-              .padding(.horizontal, 24)
-              .padding(.vertical, 6)
-              .background(Color(UIColor.systemGray6))
-              .clipShape(Capsule())
-          }
+//    VStack(alignment: .leading) {
+//        Text("栏目分类")
+//            .font(.title3)
+//            .bold()
+//        ForEach(channelmanager.channels) {channel in
+        List(channelmanager.channels) {channel in
+            Divider()
+            //AppSmall1Block()
+            // 目录ID循环
+            HStack {
+                Image(systemName: "dot.circle.and.cursorarrow")
+                    .frame(width: 24, height: 24)
+                    .accentColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                VStack(alignment: .leading) {
+                    NavigationLink(destination:ChannelListView( channelID: channel.id )){
+                        Text("\(channel.name)")
+                            .font(.system(size: 16))
+                    }
+//                    Spacer()
+//                    VStack {
+//                        Button("查看子目录 >") {}
+//                            .font(Font.system(.caption).bold())
+//                            .padding(.horizontal, 24)
+//                            .padding(.vertical, 6)
+//                            .background(Color(UIColor.systemGray6))
+//                            .clipShape(Capsule())
+//                    }
+                }
+                // 目录ID循环结束
+            }
         }
-        // 目录ID循环结束
-
-        
-      }
-    }
-    .padding(20)
+        .padding(20)
+//    }
   }
-}
-
-
-struct SuggestedAppsBlock_Previews: PreviewProvider {
-    static var previews: some View {
-        ChanneListlBlock()
-    }
 }
