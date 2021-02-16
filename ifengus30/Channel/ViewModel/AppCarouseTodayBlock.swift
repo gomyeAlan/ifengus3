96156c6ce45498c4c7fdbc801bbdee96984a8a98
wwwendydev <us68design@gmail.com>
2021-02-15 23:21:15 -0800

    修改列表页

--- ifengus30/Channel/ViewModel/AppCarouseTodayBlock.swift
+++ ifengus30/Channel/ViewModel/AppCarouseTodayBlock.swift
@@ -8,42 +8,42 @@
 import SwiftUI
 
 struct AppCarouseTodayBlock: View {
-    //    @ObservedObject var contentListManager = ContentListManager()
-    //    @StateObject var  jam = JSONArchivesModel()
-    //    @State private var archivepage: Int = 1
+        @ObservedObject var contentListManager = ContentListManager()
+        @StateObject var  jam = JSONArchivesModel()
+        @State private var archivepage: Int = 1
     
     var body: some View {
         VStack {
             VStack {
                 ChannelTopAdsBlock() //头部广告轮播
+                if jam.archiveists.isEmpty {
+                    ProgressView()
+                        .onAppear(perform: {
+                            jam.fetchSessionData(cid: 2, page: self.archivepage)
+                        })
+                } else {
+                    //列表
+                    ForEach(jam.archiveists, id: \.self){ archive in
+                        ContentListBlockSmall(archive: archive)
+                            .onAppear{
+                                if self.jam.archiveists.isLastArchive(archive) {
+                                    self.archivepage += 1
+                                    jam.fetchSessionData(cid: 2, page: self.archivepage)
+                                }
+                            }
+                            //display fetched JSON data...
+                    }
+                }
                 AppTopNewBlock() // Top News 展示
+                AppLarge3Block()
+            }
+            .onAppear {
+                self.contentListManager.getListContent(from: 2)
             }
         }
     }
 }
 
 
-//列表
-//
-//            if jam.archiveists.isEmpty {
-//                            ProgressView()
-//                                .onAppear(perform: {
-//                                    jam.fetchSessionData(cid: 2, page: self.archivepage)
-//                                })
-//                        } else {
-//                            ForEach(jam.archiveists, id: \.self){ archive in
-//                                ContentListBlockSmall(archive: archive)
-//                                    .onAppear{
-//                                        if self.jam.archiveists.isLastArchive(archive) {
-//                                            self.archivepage += 1
-//                                            jam.fetchSessionData(cid: 2, page: self.archivepage)
-//                                        }
-//                                    }
-//                                //display fetched JSON data...
-//                        }
-//                AppLarge3Block()
-//             }
-//        .onAppear {
-//            self.contentListManager.getListContent(from: 2)
 
-//      }
+
